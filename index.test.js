f616a73fc42a33834519d271ba62d209dbdd206c
ruegen <ruegen@icloud.com>
2018-07-17 12:32:32 +1000

    add subtract

--- index.test.js
+++ index.test.js
@@ -1,6 +1,11 @@
-const add = require('./index')
+const {add, subtract} = require('./index')
 
-test('test addition function', () => {
+test('addition function', () => {
     const result = add(1,3)
     expect(result).toBe(4)
+})
+
+test('subtraction function', () => {
+    const result = subtract(3, 1)
+    expect(result).toBe(2)
 })
